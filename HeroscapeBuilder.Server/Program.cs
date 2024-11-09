a4f951c9827c6c51ff8b535a538547d651e3aa12
Jacob Weigand <jacobweigand12@gmail.com>
2024-11-08 20:26:32 -0600

    more debug

--- HeroscapeBuilder.Server/Program.cs
+++ HeroscapeBuilder.Server/Program.cs
@@ -33,7 +33,7 @@ builder.Services.AddSwaggerGen(c => {
 builder.Services.AddDistributedMemoryCache();
 
 var connectionString = builder.Configuration.GetConnectionStringFromEnv("HeroscapeBuilder", "MsSqlDb");
-Console.WriteLine($"Starting migrations with connection string: {connectionString}");
+throw new Exception($"Connection string: {connectionString}");
 builder.Services.AddDbContext<HsbDbContext>((serviceProvider, options) => {
     options.UseSqlServer(connectionString, sqlOptions =>
     {
