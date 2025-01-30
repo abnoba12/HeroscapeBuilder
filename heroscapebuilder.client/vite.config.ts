5b74d5af73eb6c266bf2565b8543e473c07757e7
Jacob Weigand <jacobweigand12@gmail.com>
2025-01-30 17:39:26 -0600

    Still trying to get this working

--- heroscapebuilder.client/vite.config.ts
+++ heroscapebuilder.client/vite.config.ts
@@ -1,24 +1,7 @@
 ﻿import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
-import fs from 'fs';
 
-const certFilePath = "/certs/origin.pem";
-const keyFilePath = "/certs/origin.key";
-
-let httpsConfig: undefined | { key: Buffer; cert: Buffer } = undefined;
-
-// Ensure the certificate files exist before enabling HTTPS
-if (fs.existsSync(certFilePath) && fs.existsSync(keyFilePath)) {
-    httpsConfig = {
-        key: fs.readFileSync(keyFilePath),
-        cert: fs.readFileSync(certFilePath),
-    };
-} else {
-    console.warn("⚠️  HTTPS Certificate files not found! Ensure ~/heroscapebuilder-certs is correctly mounted.");
-}
-
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
     resolve: {
@@ -28,6 +11,6 @@ export default defineConfig({
     },
     server: {
         port: 5173,
-        https: httpsConfig, // Only enable HTTPS if certificates exist
+        https: undefined, // Always use HTTP
     },
 });
