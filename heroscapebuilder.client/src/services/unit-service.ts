54ce5d0480ec0a5ea84a5fc257ad5e1bca8acbaa
Jacob Weigand <abnoba12+github@gmail.com>
2025-12-19 15:24:17 -0600

    Fix implicit any types in client (#10)

--- heroscapebuilder.client/src/services/unit-service.ts
+++ heroscapebuilder.client/src/services/unit-service.ts
@@ -1,11 +1,13 @@
 import AxiosSingletonService from './AxiosSingletonService';
 import { debounce } from './debounce';
+import { Unit } from '../models/unit';
+import { UnitFile } from '../models/unit-file';
 
 const api = AxiosSingletonService.getInstance();
 
-export const getUnits = debounce(async () => {
+export const getUnits = debounce(async (): Promise<Unit[]> => {
     try {
-        return (await api.get(`/Unit/GetAllUnits`)).data
+        return (await api.get<Unit[]>(`/Unit/GetAllUnits`)).data;
     } catch (error) {
         console.error('Error fetching cards:', error);
         throw error;
@@ -15,7 +17,9 @@ export const getUnits = debounce(async () => {
 export const getUnitsByCardType = debounce(async (cardSize: string) => {
     try {
         const response = await getUnits();
-        return response.map(x => x.files.filter(y => y.filePurpose == `${cardSize}_Army_Card`))
+        return response.map((unit: Unit) =>
+            unit.files.filter((file: UnitFile) => file.filePurpose == `${cardSize}_Army_Card`),
+        );
 
     } catch (error) {
         console.error('Error fetching cards:', error);
